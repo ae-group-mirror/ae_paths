487c7f4fae4ba75b7b1a0439cf3177d28327202c
AndiEcker <aecker2@gmail.com>
2020-12-08 16:10:33 +0000

    V 0.1.11: allow emtpy path string to be passed into norm_path() - incremented version.

--- ae/paths.py
+++ ae/paths.py
@@ -133,7 +133,7 @@ from typing import Any, Callable, Dict, Iterable, List, Tuple, Type, Union
 from ae.base import app_name_guess, env_str, os_platform                   # type: ignore
 
 
-__version__ = '0.1.10'
+__version__ = '0.1.11'
 
 
 def add_common_storage_paths():
